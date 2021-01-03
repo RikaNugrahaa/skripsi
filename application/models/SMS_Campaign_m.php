9f49f10e60b710e5fa7ea2d298e87ee39b6ebdda
RikaNugrahaha <rikanugraha88@gmail.com>
2021-01-04 01:35:24 +0700

    tambah SMS Campaign API

---
+++ application/models/SMS_Campaign_m.php
@@ -0,0 +1,30 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+class SMS_Campaign_m extends CI_Model
+{
+
+    public function get($id = null)
+    {
+        $this->db->from('message');
+        if ($id != null) {
+            $this->db->where('message_id', $id);
+        }
+        $query = $this->db->get();
+        return $query;
+    }
+
+    public function add($post)
+    {
+        $params = [
+            'phone' => $post['recipient'],
+            'detail' => $post['message'],
+        ];
+        $this->db->insert('message', $params);
+    }
+
+    public function del($id)
+    {
+        $this->db->where('message_id', $id);
+        $this->db->delete('message');
+    }
+}
