b77d21780bed0a8f203ef3c886575c8a163f7bdb
RikaNugrahaha <rikanugraha88@gmail.com>
2021-04-02 05:11:43 +0700

    updated

--- application/views/change_password.php
+++ application/views/change_password.php
@@ -15,14 +15,13 @@
     <div class="box">
         <div class="box-header">
             <h2 class="box-title"><b>  Ubah Password</b></h2>
-            <?= $this->session->flashdata('pesan'); ?>
+           
         </div>
         <div class="box-body">
             <div class="row">
                 <div class="col-md-4 col-md-offset-4">
                     <form action="<?= site_url('User/changePassword') ?>" method="post">
-
-                       
+                      
                         <div class="form-group">
                             <label for="passwordlama">Password Lama *</label>
                             <input type="password" name="passwordlama" id="passwordlama" class=" form-control">
